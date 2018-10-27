81a4a92c6c798ccd0d8382af69df58e6f734acb0
yyokii <yyokii.h@gmail.com>
2018-10-27 13:19:08 +0900

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyB_eCC7JtiMiDY0Kwd_PLBP0aximA7g2P0',
+  authDomain: 'firestorevueapp.firebaseapp.com',
+  databaseURL: 'https://firestorevueapp.firebaseio.com',
+  projectId: 'firestorevueapp',
+  storageBucket: 'firestorevueapp.appspot.com',
+  messagingSenderId: '363884786585'
+}
