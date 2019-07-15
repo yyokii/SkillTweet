28eecb9c6a3e1a1d0538f4da79b30baeab937f87
yyokii <yyokii.h@gmail.com>
2019-07-15 23:31:27 +0900

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCZNrLBilmgu1y8SBdsNR8SA9hrl80ASag',
-  authDomain: 'skilltweetapp.firebaseapp.com',
-  databaseURL: 'https://skilltweetapp.firebaseio.com',
-  projectId: 'skilltweetapp',
-  storageBucket: 'skilltweetapp.appspot.com',
-  messagingSenderId: '689367987126'
-}
